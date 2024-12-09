43ef8fea259af9749c10133467c30c322d3f4d22
thot <135855332+thotcode@users.noreply.github.com>
2024-12-09 19:41:40 -0300

    contact form

--- assets/js/formConfirmation.js
+++ assets/js/formConfirmation.js
@@ -27,26 +27,14 @@ var onloadCallback = function() {
 };
 
 function postForm(form) {
-	var error = "Contact no valid! The form was not sent."
-	if ($(form).hasClass("nea-subscribe")) {
-		var URL = "https://docs.google.com/forms/d/1uKJWGaSlhsAd9DXm8opzyIFnicwscYdenYhCyNB-KWY/formResponse";
-		var thanks = "Thanks for subscribing. We will be in touch soon.";
-	} else if ($(form).hasClass("nea-investor")) {
-		var URL = "https://docs.google.com/forms/d/1js4c8uf---mPzKOpfOQR-8rYkR-DQu9FPJgIEgzDOhk/formResponse";
-		var thanks = "Thank you! We will be in touch soon.";
-	} else if ($(form).hasClass("nea-contact")) {
+	var error = "Contact no valid! The form was not sent.";
+	if ($(form).hasClass("nea-contact")) {
 		var URL = "https://docs.google.com/forms/d/1CpamEupan42CHwtJN1VqJnjgoQGud8SI2WAb9XCVqPU/formResponse";
 		var thanks = "Thanks for contacting us. We will be in touch soon.";
 	}
-	/*else if ($(form).hasClass("nea-coupon")){
-		var URL = "https://docs.google.com/forms/d/14YNj4cC9nPhMl1vIzrUkfh3NW4Xbwyoqkcru3Wgw6pI/formResponse";
-		var thanks = "Thank you. You will receive soon a special discount.";
-	}*/
-
 
 	var email = $("input#form-field-nea_email", form).val();
 	var name = $("input#form-field-nea_name", form).val();
-	var attend = $("input[name='investor-sub']:checked", form).val();
 	var message = $("#form-field-nea_message", form).val();
 	var source = "neahtid.com";
 
@@ -69,7 +57,6 @@ function postForm(form) {
 		data: {
 			"entry.1155430950": email,
 			"entry.545860963": name,
-			"entry.512963360": attend,
 			"entry.1997542075": message,
 			"entry.26006045": source
 		},
@@ -81,13 +68,11 @@ function postForm(form) {
 				console.warn("statusCode: 0");
 				$('button[type=submit]', form).disabled = false;
 				$('.fa-inactive', form).removeClass('fa-active');
-				// console.trace();
 			},
 			200: function() {
 				$(form).html('<p class="nea-form-sent">' + thanks + '</p>');
 				$('button[type=submit]', form).disabled = false;
 				$('.fa-inactive', form).removeClass('fa-active');
-				// console.trace();
 			}
 		}
 	});
