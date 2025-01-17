37f10f7a845a47c677726829d7ef82a3e751db70
thot <135855332+thotcode@users.noreply.github.com>
2025-01-17 16:49:03 -0300

    hardware modal and services links

--- assets/js/custom/p_services_5_hardware.js
+++ assets/js/custom/p_services_5_hardware.js
@@ -19,3 +19,23 @@ $(document).ready(function () {
         this.reset();
     });
 });
+
+const contactModal = document.getElementById('contactModal')
+if (contactModal) {
+  contactModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget;
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-consultation');
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = contactModal.querySelector('.modal-title');
+    const modalConsultationTtitle = contactModal.querySelector('#data-consultation');
+    const modalBodyInput = contactModal.querySelector('.modal-body input[name="consultation"]');
+
+    modalConsultationTtitle.textContent = `${recipient}`;
+    modalBodyInput.value = recipient;
+  })
+}
